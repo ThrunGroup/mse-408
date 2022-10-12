8cbb145bb821858d9b8f16867d43d9c68c4a2cef
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-10-12 12:12:38 -0700

    adding basic setup/tests

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -1,25 +1,76 @@
 #!/usr/bin/env python3
 import argparse
+import logging
+import multiprocessing as mp
 import sys
 
 import numpy as np
 
-# MCMC Questions:
-# 1. When do you stop sampling?
-#   - You reach N valid samples
-#   - p(X in A) approaches it's true probability
 # GFN Questions:
 # 1. How do you design the reward function? R(x)=1 for X in A and 0 otherwise?
 # 2. When do you stop training?
 
 
-def mcmc(starting_state, p_success, lower, upper):
+def mcmc(
+    starting_state: int = 5,
+    lower: int = 0,
+    upper: int = 10,
+    p_success: float = 0.49,
+    n_burn_in: int = int(1e4),
+    epsilon: float = 1e-6,
+    log_every_n: int = int(100),
+) -> tuple[int, float, list[list[int]]]:
     if not starting_state > lower and starting_state < upper:
         raise InvalidStateError("starting state must lie between [lower, upper]")
+    is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
+    n = 1
+    p_win = is_win
+    traj_cond_win = []
+    if is_win:
+        traj_cond_win += [traj]
+    delta = 0
+    while n < n_burn_in:
+        is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
+        n += 1
+        delta = (is_win - p_win) / n
+        p_win += delta
+        if is_win:
+            traj_cond_win += [traj]
+        if n % log_every_n == 0:
+            logging.debug(f"P(WIN)={p_win:.4f}")
+    while delta > epsilon:
+        is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
+        n += 1
+        delta = (is_win - p_win) / n
+        p_win += delta
+        if is_win:
+            traj_cond_win += [traj]
+        if n % log_every_n == 0:
+            logging.debug(f"P(WIN)={p_win:.4f}")
+    return n, p_win, traj_cond_win
 
 
-def sample_naive(_state, p_success):
-    return np.random.binomial(1, p_success)
+def sample_trajectory(starting_state, lower, upper, p_success):
+    traj = [starting_state]
+    while traj[-1] > lower and traj[-1] < upper:
+        delta = (np.random.binomial(1, p_success) - 0.5) * 2
+        traj += [traj[-1] + delta]
+    return int(traj[-1] == upper), traj
+
+
+def mcmc(
+    starting_state: int = 5,
+    lower: int = 0,
+    upper: int = 10,
+    p_success: float = 0.49,
+    n_burn_in: int = int(1e4),
+    epsilon: float = 1e-6,
+    log_every_n: int = int(100),
+) -> tuple[int, float, list[list[int]]]:
+    if not starting_state > lower and starting_state < upper:
+        raise InvalidStateError("starting state must lie between [lower, upper]")
+    # TODO(danj): implement
+    return None, None, None
 
 
 def parse_args(argv):
@@ -30,28 +81,50 @@ def parse_args(argv):
         "-s",
         "--starting_state",
         help="starting state, must be in [lower, upper], defaults to floor((upper-lower)/2)",
-        dtype=int,
-    )
-    parser.add_argument(
-        "-p",
-        "--p_success",
-        help="probability of success",
-        dtype=float,
-        default=0.49,
+        type=int,
+        default=5,
     )
     parser.add_argument(
         "-l",
         "--lower",
         help="lower bound",
-        dtype=int,
+        type=int,
         default=0,
     )
     parser.add_argument(
         "-u",
         "--upper",
         help="upper bound",
-        dtype=int,
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "-p",
+        "--p_success",
+        help="probability of success",
+        type=float,
+        default=0.49,
+    )
+    parser.add_argument(
+        "-nb",
+        "--n_burn_in",
+        help="number of burn in samples",
+        type=int,
         default=100,
+    )
+    parser.add_argument(
+        "-e",
+        "--epsilon",
+        help="determines when sampling should stop (delta < epsilon)",
+        type=float,
+        default=1e-6,
+    )
+    parser.add_argument(
+        "-logn",
+        "--log_every_n",
+        help="log estimate every n samples",
+        type=int,
+        default=1000,
     )
     args = parser.parse_args(argv[1:])
     if not args.starting_state:
@@ -65,4 +138,14 @@ class InvalidStateError(Exception):
 
 if __name__ == "__main__":
     args = parse_args(sys.argv)
-    mcmc(args.starting_state, args.p_success, args.lower, args.upper)
+    print(
+        mcmc(
+            args.starting_state,
+            args.lower,
+            args.upper,
+            args.p_success,
+            args.n_burn_in,
+            args.epsilon,
+            args.log_every_n,
+        )
+    )
---
+++ tests/test_gamblers_ruin.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import numpy as np
+
+from gamblers_ruin import mcmc
+
+
+def test_mcmc():
+    starting_state = 5
+    lower = 0
+    upper = 10
+    p_success = 0.49
+    _, est_p_win, _ = mcmc(starting_state, lower, upper, p_success)
+    true_p_win = p_win(starting_state, upper, p_success)
+    assert np.isclose(
+        est_p_win - true_p_win, 0, atol=0.01
+    ), "Vanilla MCMC produced an inaccurate estimate!"
+
+
+def test_exp_tilt_mcmc():
+    starting_state = 5
+    lower = 0
+    upper = 10
+    p_success = 0.49
+    _, est_p_win, _ = exp_tilt_mcmc(starting_state, lower, upper, p_success)
+    true_p_win = p_win(starting_state, upper, p_success)
+    assert np.isclose(
+        est_p_win - true_p_win, 0, atol=0.01
+    ), "Exponential Tilt MCMC produced an inaccurate estimate!"
+
+
+def p_win(starting_state, upper, p_success):
+    if p_success == 0.5:
+        return starting_state / upper
+    q_div_p = (1 - p_success) / p_success
+    return (1 - q_div_p**starting_state) / (1 - q_div_p**upper)
