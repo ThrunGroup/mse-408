0e64c0213b52e0b2d04e063546ef7ed6377fb969
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-10-17 15:33:55 -0700

    cleanup, using this repo for code, overleaf for docs

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -3,124 +3,160 @@ import argparse
 import logging
 import multiprocessing as mp
 import sys
+from typing import Callable
 
 import numpy as np
 
-# GFN Questions:
-# 1. How do you design the reward function? R(x)=1 for X in A and 0 otherwise?
-# 2. When do you stop training?
+logging.basicConfig(
+    format="[%(levelname)s] %(asctime)s: %(message)s", level=logging.DEBUG
+)
+
+
+def p_hit_b(a, b, s0, p):
+    """True probability of hitting `b` before `-a` with success probability `p`."""
+    if p == 0.5:
+        return s0 / (b + a)
+    q_div_p = (1 - p) / p
+    return (1 - q_div_p**b) / (1 - q_div_p ** (b + a))
+
+
+class InvalidStateError(Exception):
+    pass
+
+
+def sampler(
+    a: int,
+    b: int,
+    s0: int,
+    p: float,
+    q: float,
+    theta: float,
+    sample: Callable[[int, int, int, float, float, float], None],
+    queue: mp.Queue,
+    stop: mp.Event,
+) -> None:
+    """A sampling worker for multiprocessing."""
+    while not stop.is_set():
+        queue.put(sample(a, b, s0, p, q, theta))
+
+
+def sample(a, b, s0, p, *rest):
+    """A vanilla MCMC sampling."""
+    t = [s0]
+    while t[-1] > -a and t[-1] < b:
+        delta = 2 * np.random.binomial(1, p) - 1
+        t += [t[-1] + delta]
+    return t
+
+
+def exp_tilt_sample(a, b, s0, p, q, theta):
+    """Exponentially tilted MCMC sampling."""
+    p_tilt = p * np.exp(theta) / (q * np.exp(-theta) + p * np.exp(theta))
+    t = [s0]
+    while t[-1] > -a and t[-1] < b:
+        x = 2 * np.random.binomial(1, p_tilt) - 1
+        t += [t[-1] + x]
+    return t
+
+
+def exp_tilt_weight(t, theta, psi_theta):
+    """Importance sampling weight for exponential tilting."""
+    s = t[-1] - t[0]
+    n = len(t)
+    return 1 / np.exp(theta * s - n * psi_theta)
 
 
 def mcmc(
-    starting_state: int = 5,
-    lower: int = 0,
-    upper: int = 10,
-    p_success: float = 0.49,
-    n_burn_in: int = int(1e4),
+    a: int = 5,
+    b: int = 5,
+    s0: int = 0,
+    p: float = 0.49,
+    n_burn: int = 100,
     epsilon: float = 1e-6,
-    log_every_n: int = int(100),
+    sample: Callable[[int, int, int, float, float, float], list[int]] = sample,
+    weight: Callable[[list[int], float, float], float] = lambda t, theta, psi_theta: 1,
+    log_every_n: int = 1000,
+    n_samplers: int = mp.cpu_count(),
 ) -> tuple[int, float, list[list[int]]]:
-    if not starting_state > lower and starting_state < upper:
-        raise InvalidStateError("starting state must lie between [lower, upper]")
-    is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
-    n = 1
-    p_win = is_win
-    traj_cond_win = []
-    if is_win:
-        traj_cond_win += [traj]
+    """MCMC sampling for Gambler's Ruin.
+
+    Args:
+        a (int): `-a` is the lower bound.
+        b (int): `b` is the upper bound.
+        s0 (int): The inital state.
+        p (float): The probability of success on each bet.
+        n_burn (int): Number of burn in samples.
+        epsilon (float): Absolute error for estimate that determines when to stop sampling.
+        sample (Callable[[int, int, int, float, float, float], list[int]]): A
+            function that takes (a, b, s0, p, q, theta) and returns a sample
+            trajectory.
+        weight (Callable[[list[int], float, float], float]): Calculates the
+            importance weight for a sample.
+        log_every_n (int): Log estimate every `log_every_n` samples.
+        n_samplers (int): Number of samplers to run in parallel.
+
+    Returns:
+        tuple[int, float, list[int]]: Returns a tuple of (number of samples,
+            estimate for `P(hit b)`, list of trajectories conditional on hitting b).
+    """
+    if not (s0 > -a and s0 < b):
+        raise InvalidStateError("s0 must lie in (-a, b)")
+
+    q = 1 - p
+    theta = np.log(q / p)
+    psi_theta = np.log(q * np.exp(-theta) + p * np.exp(theta))
+    p_hit_b = 0
+    n = 0
     delta = 0
-    while n < n_burn_in:
-        is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
-        n += 1
-        delta = (is_win - p_win) / n
-        p_win += delta
-        if is_win:
-            traj_cond_win += [traj]
-        if n % log_every_n == 0:
-            logging.debug(f"P(WIN)={p_win:.4f}")
-    while delta > epsilon:
-        is_win, traj = sample_trajectory(starting_state, lower, upper, p_success)
-        n += 1
-        delta = (is_win - p_win) / n
-        p_win += delta
-        if is_win:
-            traj_cond_win += [traj]
-        if n % log_every_n == 0:
-            logging.debug(f"P(WIN)={p_win:.4f}")
-    return n, p_win, traj_cond_win
+    t_cond_b = []
 
-
-def sample_trajectory(starting_state, lower, upper, p_success):
-    traj = [starting_state]
-    while traj[-1] > lower and traj[-1] < upper:
-        delta = (np.random.binomial(1, p_success) - 0.5) * 2
-        traj += [traj[-1] + delta]
-    return int(traj[-1] == upper), traj
-
-
-def exp_tilt_mcmc(
-    starting_state: int = 5,
-    lower: int = 0,
-    upper: int = 10,
-    p_success: float = 0.49,
-    n_burn_in: int = int(1e4),
-    epsilon: float = 1e-6,
-    log_every_n: int = int(100),
-) -> tuple[int, float, list[list[int]]]:
-    if not starting_state > lower and starting_state < upper:
-        raise InvalidStateError("starting state must lie between [lower, upper]")
-    traj_cond_win = []
-    is_win, traj = exp_tilt_sample_trajectory(starting_state, lower, upper, p_success)
-    if is_win:
-        traj_cond_win += [traj]
-    d = upper - starting_state
-    n = 1
-    q = 1 - p_success
-    theta = np.log(q / p_success)  # analytically optimal theta
-    # theta = np.log(p_success / q)  # analytically optimal theta
-    psi_theta = np.log(q + p_success * np.exp(theta))
-    p_win = is_win = is_win / np.exp(theta * d - len(traj) * psi_theta)
-    delta = 0
-    while n < n_burn_in:
-        is_win, traj = exp_tilt_sample_trajectory(
-            starting_state, lower, upper, p_success
+    queue = mp.Queue()
+    stop = mp.Event()
+    samplers = []
+    for _ in range(n_samplers):
+        sampler = mp.Process(
+            target=sample,
+            args=(
+                a,
+                b,
+                s0,
+                p,
+                q,
+                theta,
+                sample,
+                queue,
+                stop,
+            ),
         )
-        if is_win:
-            traj_cond_win += [traj]
-        n += 1
-        # reweight by importance sampling weight
-        is_win = is_win / np.exp(theta * d - len(traj) * psi_theta)
-        delta = (is_win - p_win) / n
-        p_win += delta
-        if n % log_every_n == 0:
-            logging.debug(f"P(WIN)={p_win:.4f}")
-    while delta > epsilon:
-        is_win, traj = exp_tilt_sample_trajectory(
-            starting_state, lower, upper, p_success
-        )
-        if is_win:
-            traj_cond_win += [traj]
-        n += 1
-        # reweight by importance sampling weight
-        is_win = is_win / np.exp(theta * d - len(traj) * psi_theta)
-        delta = (is_win - p_win) / n
-        p_win += delta
-        if n % log_every_n == 0:
-            logging.debug(f"P(WIN)={p_win:.4f}")
-    return n, p_win, traj_cond_win
+        samplers.append(sampler)
+        sampler.start()
 
+    def update_estimate():
+        nonlocal a, b, s0, p, q, theta, psi_theta, p_hit_b, n, delta, t_cond_b
+        t = sample(a, b, s0, p, q, theta)
+        w = weight(t, theta, psi_theta)
+        hit_b = t[-1] == b
+        p_hat = w * hit_b
+        n += 1
+        delta = (p_hat - p_hit_b) / n
+        p_hit_b += delta
+        if hit_b:
+            t_cond_b.append(t)
 
-def exp_tilt_sample_trajectory(starting_state, lower, upper, p_success):
-    traj = [starting_state]
-    q = 1 - p_success
-    theta = np.log(q / p_success)  # analytically optimal theta
-    # theta = np.log(p_success / q)  # analytically optimal theta
-    pt = p_success * np.exp(theta)
-    p = pt / (q + pt)
-    while traj[-1] > lower and traj[-1] < upper:
-        delta = 2 * np.random.binomial(1, p) - 1
-        traj += [traj[-1] + delta]
-    return int(traj[-1] == upper), traj
+    while n < n_burn:
+        update_estimate()
+        if n % log_every_n == 0:
+            logging.debug(f"P(hit b): {p_hit_b:0.5f} ({n} samples)")
+
+    while np.abs(delta) > epsilon:
+        update_estimate()
+        if n % log_every_n == 0:
+            logging.debug(f"P(hit b): {p_hit_b:0.5f} ({n} samples)")
+
+    stop.set()
+    [sampler.join() for sampler in samplers]
+    queue.close()
+    return n, p_hit_b, t_cond_b
 
 
 def parse_args(argv):
@@ -128,36 +164,32 @@ def parse_args(argv):
         prog=argv[0], formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "-s",
-        "--starting_state",
-        help="starting state, must be in [lower, upper], defaults to floor((upper-lower)/2)",
-        type=int,
-        default=5,
-    )
-    parser.add_argument(
-        "-l",
-        "--lower",
-        help="lower bound",
+        "-s0",
+        help="starting state, must be in (-a, b), defaults to floor((b-(-a))/2)",
         type=int,
         default=0,
     )
     parser.add_argument(
-        "-u",
-        "--upper",
-        help="upper bound",
+        "-a",
+        help="positive integer, -a = lower bound",
         type=int,
-        default=10,
+        default=5,
+    )
+    parser.add_argument(
+        "-b",
+        help="positive integer, b = upper bound",
+        type=int,
+        default=5,
     )
     parser.add_argument(
         "-p",
-        "--p_success",
         help="probability of success",
         type=float,
         default=0.49,
     )
     parser.add_argument(
         "-nb",
-        "--n_burn_in",
+        "--n_burn",
         help="number of burn in samples",
         type=int,
         default=1000,
@@ -165,46 +197,52 @@ def parse_args(argv):
     parser.add_argument(
         "-e",
         "--epsilon",
-        help="determines when sampling should stop (delta < epsilon)",
+        help="determines when sampling should stop (|delta| < epsilon)",
         type=float,
         default=1e-6,
+    )
+    parser.add_argument(
+        "-et",
+        "--exponential_tilt",
+        help="Use exponential tilting",
+        action="store_true",
     )
     parser.add_argument(
         "-logn",
         "--log_every_n",
         help="log estimate every n samples",
         type=int,
-        default=10000,
+        default=1000,
+    )
+    parser.add_argument(
+        "-ns",
+        "--n_samplers",
+        help="number of parallel samplers to use",
+        type=int,
+        default=mp.cpu_count(),
     )
     args = parser.parse_args(argv[1:])
-    if not args.starting_state:
-        args.starting_state = int((args.upper - args.lower) / 2)
+    if not args.s0:
+        args.s0 = int((args.b + -args.a) / 2)
     return args
-
-
-class InvalidStateError(Exception):
-    pass
 
 
 if __name__ == "__main__":
     args = parse_args(sys.argv)
-    _, p_win, _ = mcmc(
-        args.starting_state,
-        args.lower,
-        args.upper,
-        args.p_success,
-        args.n_burn_in,
+    weight = lambda t, theta, psi_theta: 1
+    if args.exponential_tilt:
+        sample = exp_tilt_sample
+        weight = exp_tilt_weight
+    n, p_hit_b, _ = mcmc(
+        args.a,
+        args.b,
+        args.s0,
+        args.p,
+        args.n_burn,
         args.epsilon,
+        sample,
+        weight,
         args.log_every_n,
+        args.n_samplers,
     )
-    print(f"mcmc: P(WIN)={p_win}")
-    _, p_win, _ = exp_tilt_mcmc(
-        args.starting_state,
-        args.lower,
-        args.upper,
-        args.p_success,
-        args.n_burn_in,
-        args.epsilon,
-        args.log_every_n,
-    )
-    print(f"exp tilt mcmc: P(WIN)={p_win}")
+    print(f"P(hit b): {p_hit_b:0.5f} ({n} samples)")
--- tests/test_gamblers_ruin.py
+++ tests/test_gamblers_ruin.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 import numpy as np
 
-from gamblers_ruin import exp_tilt_mcmc, mcmc
+from gamblers_ruin import mcmc, p_hit_b
 
 
-def test_mcmc():
-    starting_state = 5
-    lower = 0
-    upper = 10
-    p_success = 0.49
-    _, est_p_win, _ = mcmc(starting_state, lower, upper, p_success)
-    true_p_win = p_win(starting_state, upper, p_success)
-    assert np.isclose(
-        est_p_win - true_p_win, 0, atol=0.01
-    ), "Vanilla MCMC produced an inaccurate estimate!"
+def test_vanilla_mcmc():
+    a, b, s0 = 5, 5, 0
+    for p in [0.49, 0.25, 0.1]:
+        n, p_hit_b_est, _ = mcmc(a, b, s0, p)
+        p_hit_b_true = p_hit_b(a, b, s0, p)
+        assert np.isclose(
+            p_hit_b_est, p_hit_b_true, rtol=0.1
+        ), f"Vanilla MCMC produced an inaccurate estimate for p={p} ({n} samples)!"
 
 
 def test_exp_tilt_mcmc():
-    starting_state = 5
-    lower = 0
-    upper = 10
-    p_success = 0.49
-    _, est_p_win, _ = exp_tilt_mcmc(starting_state, lower, upper, p_success)
-    true_p_win = p_win(starting_state, upper, p_success)
-    assert np.isclose(
-        est_p_win - true_p_win, 0, atol=0.01
-    ), "Exponential Tilt MCMC produced an inaccurate estimate!"
-
-
-def p_win(starting_state, upper, p_success):
-    if p_success == 0.5:
-        return starting_state / upper
-    q_div_p = (1 - p_success) / p_success
-    return (1 - q_div_p**starting_state) / (1 - q_div_p**upper)
+    a, b, s0 = 5, 5, 0
+    for p in [0.49, 0.25, 0.1]:
+        n, p_hit_b_est, _ = mcmc(a, b, s0, p, epsilon=1e-7)
+        p_hit_b_true = p_hit_b(a, b, s0, p)
+        assert np.isclose(
+            p_hit_b_est, p_hit_b_true, rtol=0.1
+        ), f"Exponential Tilt MCMC produced an inaccurate estimate for p={p} ({n} samples)!"
