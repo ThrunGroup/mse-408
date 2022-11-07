49666e713624bfb73013229414782639a15b63ed
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-11-07 14:03:38 -0800

    working on env

--- py/post/posterior.py
+++ py/post/posterior.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import namedtuple
 from typing import Callable, Union
 
 import numpy as np
@@ -17,18 +18,66 @@ def flow_matching_loss(
     return torch.log(ratio).pow(2).mean()
 
 
-class PosteriorEnv:
-    def __init__(self, reward: Callable):
-        self.reward = reward
-        pass
+Param = namedtuple("Param", ["name", "min", "max", "n"])
 
-    def step(**kwargs):
-        pass
+
+class PosteriorEnv:
+    def __init__(
+        self,
+        *params,
+        data: np.ndarray,
+        likelihood: Callable,
+        batch_size=32,
+    ):
+        self.params = params
+        self.value = [np.linspace(p.min, p.max, p.n) for p in params]
+        self.data = data
+        self.likelihood = likelihood
+        self.batch_size = batch_size
+        self.s0 = np.zeros(len(params))
+
+    def reward(self, state: np.ndarray) -> float:
+        d = {self.params[i].name: self.value[i][s] for i, s in enumerate(state)}
+        n = self.data.shape[0]
+        idx = np.random.randint(n, size=self.batch_size)
+        d["x"] = self.data[idx]
+        return self.likelihood(**d)
+
+    def step(self, state: np.ndarray, action: int):
+        if action < len(self.params):
+            state[action] += 1
+        is_stop_action = action == len(self.params)
+        is_terminal = state[action] == self.params[action].n - 1
+        done = is_stop_action or is_terminal
+        reward = 0 if not done else self.reward(state)
+        return state, reward
+
+    def parent_transitions(self, state: np.ndarray, action: int):
+        is_stop_action = action == len(self.params)
+        is_terminal = state[action] == self.params[action].n - 1
+        if is_stop_action:
+            # When the stop action is used, the state does not change; in
+            # this case, the "parent" of the current (terminal) state is
+            # itself and the action used to get there is the stop action.
+            return [state], [action]
+        parents = []
+        actions = []
+        for action in range(len(self.params)):
+            if state[action] > 0:
+                parent = np.copy(state)
+                parent[action] -= 1
+                # can't have a terminal parent
+                is_terminal = state[action] == self.params[action].n - 1
+                if is_terminal:
+                    continue
+                parents.append(parent)
+                actions.append(action)
+        return np.array(parents), np.array(actions)
 
 
 class PosteriorGFN(pl.LightningModule):
     def __init__(self):
         pass
 
-    def sample(self, n: int) -> torch.Tensor:
+    def sample(self, n: int) -> np.ndarray:
         pass
--- py/post/tests/test_posterior.py
+++ py/post/tests/test_posterior.py
@@ -2,8 +2,9 @@
 import numpy as np
 import pytest
 from pytorch_lightning import Trainer
+from scipy import stats
 
-from posterior import PosteriorEnv, PosteriorGFN, flow_matching_loss
+from posterior import Param, PosteriorEnv, PosteriorGFN, flow_matching_loss
 
 
 @pytest.mark.parametrize(
@@ -20,12 +21,40 @@ def test_flow_matching_loss(inflow, outflow, delta, loss):
     assert np.isclose(est_loss, loss), "Incorrect flow matching loss!"
 
 
-@pytest.mark.parameterize("p", [0.1, 25, 0.49])
-def test_bernoulli_posterior(p):
-    env = PosteriorEnv()
-    gfn = PosteriorGFN(env)
-    trainer = Trainer(max_steps=1000)
-    trainer.fit(gfn)
-    samples = gfn.sample(n=1000).numpy()
-    assert np.isclose(samples.mean(), p), "Estimated p is incorrect!"
-    assert np.isclose(samples.var(), p * (1 - p)), "Estimated variance is incorrect!"
+def test_posterior_env_bernoulli():
+    np.random.seed(0)
+    p, batch_size = 0.25, 2
+    param_p = Param(name="p", min=0.01, max=0.99, n=99)
+    env = PosteriorEnv(
+        param_p,
+        data=np.zeros(batch_size),
+        likelihood=lambda x, p: stats.bernoulli.pmf(x, p).prod(),
+        batch_size=32,
+    )
+    s1 = env.step(env.s0, 0)
+    assert env.reward(env.s0)[0] == 0.9801, "Incorrect reward!"
+    assert env.reward(s1)[0].round(4) == 0.9603, "Incorrect reward!"
+    env.data = stats.bernoulli.rvs(p, size=batch_size)
+    # gfn = PosteriorGFN(env)
+    # trainer = Trainer(max_steps=1000)
+    # trainer.fit(gfn)
+    # samples = gfn.sample(n=1000).numpy()
+    # assert np.isclose(samples.mean(), p), "Estimated p is incorrect!"
+    # assert np.isclose(samples.var(), p * (1 - p)), "Estimated variance is incorrect!"
+
+
+def test_posterior_env_normal():
+    np.random.seed(0)
+    mu, sigma, batch_size = 2, 2, 32
+    param_mu = Param(name="mu", min=-16, max=16, n=100)
+    param_sigma = Param(name="sigma", min=0.01, max=10, n=100)
+    data = stats.norm.rvs(mu, sigma, size=64)
+    env = PosteriorEnv(
+        param_mu,
+        param_sigma,
+        data=data,
+        likelihood=lambda x, mu, sigma: stats.norm.pmf(x, mu, sigma).prod(),
+        batch_size=batch_size,
+    )
+    assert env.s0[0] == 0, "Incorrect s0!"
+    assert env.s0[1] == 0, "Incorrect s0!"
