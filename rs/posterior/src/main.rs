126d27d2bc6ac1512e2ec2d8c556720cbba03cc6
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-11-22 17:16:48 -0800

    meh

--- rs/posterior/src/main.rs
+++ rs/posterior/src/main.rs
@@ -1,13 +1,3 @@
-use std::ops::Index;
-use tch::Tensor;
-
-pub trait Flows<F, Idx>: Index<Idx, Output = F> + Iterator<Item = F> {}
-
-impl Flows<Tensor, Tensor> for Tensor {
-    // TODO
-}
-
 fn main() {
-    let t: Box<dyn Flows<Tensor, Tensor>> =
-        Box::new(Tensor::of_slice(&[3, 1, 4, 1, 5]));
+    println!("Hello, world!");
 }
