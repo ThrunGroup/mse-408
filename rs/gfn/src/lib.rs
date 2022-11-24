1993ad9b95a0619e7db7248135db7bd787e1237f
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-11-23 17:01:29 -0800

    updates

--- rs/gfn/src/lib.rs
+++ rs/gfn/src/lib.rs
@@ -1,6 +1,3 @@
-use std::collections::HashMap;
-use tch::Tensor;
-
 /// This defines the contract for all states.
 pub trait State {
     fn is_initial(&self) -> bool;
@@ -12,25 +9,30 @@ pub trait Action {
     fn is_terminal(&self) -> bool;
 }
 
-pub trait Environment<S, A: Action, R, Idx> {
-    fn step(&self, state: S, action: A) -> Step<S, A, R>;
-    fn reward(&self, transition: Transition<S, A>) -> R;
+pub trait Environment<S: State, A: Action, R> {
+    fn step(&self, state: &S, action: &A) -> Step<S, A, R>;
+    fn reward(&self, transition: &Transition<S, A>) -> R;
 }
 
-pub struct Transition<S, A> {
+pub struct Transition<S: State, A: Action> {
     pub state: S,
     pub action: A,
     pub next_state: S,
 }
 
-pub struct Step<S, A, R> {
+pub struct Step<S: State, A: Action, R> {
     pub transition: Transition<S, A>,
     pub reward: R,
 }
 
+impl<S: State, A: Action> Transition<S, A> {
+    pub fn is_terminal(&self) -> bool {
+        self.next_state.is_terminal() || self.action.is_terminal()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn test_trajectory_balance_loss() {}
--- rs/gfn/src/main.rs
+++ rs/gfn/src/main.rs
@@ -1,13 +1,85 @@
-use posterior::{Environment, State};
+use gfn::{Action, Environment, State, Step, Transition};
+use tch::Tensor;
 
-
-type HypergridState = Vec<usize>;
-
-struct Hypergrid {
-    initial_state: HypergridState,
+#[derive(Debug, Copy)]
+struct HypergridState {
+    coordinate: Tensor,
+    n_per_dim: usize,
+    x_min: f64,
+    x_max: f64,
 }
 
-impl 
+#[derive(Debug, Copy)]
+struct HypergridAction {
+    direction: usize,
+    terminal: usize,
+}
+
+struct Hypergrid {}
+
+impl HypergridState {
+    fn apply(&self, action: usize) -> Result<Self, &str> {
+        if action.is_terminal() {
+            return self.copy();
+        }
+        let mut coordinate = self.coordinate.copy();
+        coordinate[action.direction] += 1;
+        if coordinate[action.direction] < self.n_per_dim {
+            Ok(Self {
+                coordinate,
+                ..*self
+            })
+        } else {
+            Err("Attempt to advance to an invalid state.")
+        }
+    }
+
+    fn initial(&self) -> Self {
+        Self {
+            coordinate: self.coordinate.zeros_like(),
+            ..*self
+        }
+    }
+}
+
+impl State for HypergridState {
+    fn is_initial(&self) -> bool {
+        self.coordinate.sum(self.coordinate.kind())
+    }
+
+    fn is_terminal(&self) -> bool {
+        self.coordinate.contains(&(&self.n_per_dim - 1))
+    }
+}
+
+impl Action for HypergridAction {
+    fn is_terminal(&self) -> bool {
+        self.action.contains(&self.terminal)
+    }
+}
+
+impl<S: State, A: Action, R> Environment<S, A, R> for Hypergrid {
+    fn step(
+        &self,
+        state: &HypergridState,
+        action: &HypergridAction,
+    ) -> Result<Step<S, A, R>, &str> {
+        let transition = Transition {
+            state: state.copy(),
+            action: action.copy(),
+            next_state: state.apply(action)?,
+        };
+        let mut reward = 0;
+        if transition.is_terminal() {
+            reward = self.reward(state);
+        }
+        Ok(Step { transition, reward })
+    }
+
+    fn reward(state: &HypergridState) -> f64 {
+        5.0
+    }
+}
 
 fn main() {
     println!("Hello, world!");
