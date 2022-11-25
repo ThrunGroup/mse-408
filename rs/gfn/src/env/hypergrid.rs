d093184892f2881636a02d7f9b48d4bae96e810b
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-11-25 10:06:37 -0800

    updates

--- rs/gfn/src/env/hypergrid.rs
+++ rs/gfn/src/env/hypergrid.rs
@@ -1,21 +1,22 @@
 use crate::{
     Action, Environment, InvalidTransitionError, State, Step, Transition,
 };
-use tch::{Device, Tensor};
+use ndarray::{Array, Array1, ArrayView1, Ix1};
 
 // TODO(danj): rewards function
 // TODO(danj): loss function
 // TODO(danj): training loop
 
+#[derive(Debug)]
 pub struct HypergridState {
-    coordinate: Tensor,
+    coordinate: Array1<usize>,
     n_per_dim: usize,
 }
 
 impl Clone for HypergridState {
     fn clone(&self) -> Self {
         Self {
-            coordinate: self.coordinate.copy(),
+            coordinate: self.coordinate.clone(),
             ..*self
         }
     }
@@ -23,22 +24,18 @@ impl Clone for HypergridState {
 
 impl State for HypergridState {
     fn is_initial(&self) -> bool {
-        self.coordinate.sum(self.coordinate.kind()) == Tensor::from(0)
+        self.coordinate.sum() == 0
     }
 
     fn is_terminal(&self) -> bool {
-        self.coordinate.eq((self.n_per_dim - 1) as i64).any().into()
+        self.coordinate.iter().any(|x| *x == self.n_per_dim - 1)
     }
 }
 
 impl HypergridState {
     pub fn new(n_dims: usize, n_per_dim: usize) -> Self {
-        let device = Device::cuda_if_available();
         Self {
-            coordinate: Tensor::zeros(
-                &[n_dims as i64],
-                (tch::Kind::Int64, device),
-            ),
+            coordinate: Array1::zeros(n_dims),
             n_per_dim,
         }
     }
@@ -50,14 +47,9 @@ impl HypergridState {
         if action.is_terminal() {
             return Ok(self.clone());
         }
-        let coordinate = self.coordinate.index_add(
-            0,
-            &Tensor::from(action.direction as i64),
-            &Tensor::from(1),
-        );
-        if i64::from(coordinate.get(action.direction as i64))
-            < self.n_per_dim as i64
-        {
+        let mut coordinate = self.coordinate.clone();
+        coordinate[action.direction] += 1;
+        if coordinate[action.direction] < self.n_per_dim {
             Ok(Self {
                 coordinate,
                 ..*self
@@ -80,26 +72,17 @@ impl Action for HypergridAction {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct Hypergrid {
     s0: HypergridState,
-    x_space: Tensor,
-    r: fn(&Tensor) -> Tensor,
-}
-
-impl Clone for Hypergrid {
-    fn clone(&self) -> Self {
-        Self {
-            s0: self.s0.clone(),
-            x_space: self.x_space.copy(),
-            ..*self
-        }
-    }
+    x_space: Array1<f64>,
+    r: fn(x: &ArrayView1<f64>) -> f64,
 }
 
 impl Environment for Hypergrid {
     type S = HypergridState;
     type A = HypergridAction;
-    type R = Tensor;
+    type R = f64;
 
     fn s0(&self) -> Self::S {
         self.s0.clone()
@@ -122,7 +105,7 @@ impl Environment for Hypergrid {
     fn reward(
         &self,
         transition: &Transition<HypergridState, HypergridAction>,
-    ) -> Tensor {
+    ) -> f64 {
         let x = self.x(&transition.next_state);
         (self.r)(&x)
     }
@@ -134,32 +117,25 @@ impl Hypergrid {
         n_per_dim: usize,
         x_min: f64,
         x_max: f64,
-        r: fn(&Tensor) -> Tensor,
+        r: fn(&ArrayView1<f64>) -> f64,
     ) -> Self {
-        let device = Device::cuda_if_available();
-        let kind = tch::Kind::Int64;
         Self {
             s0: HypergridState::new(n_dims, n_per_dim),
-            x_space: Tensor::linspace(
-                x_min,
-                x_max,
-                n_per_dim as i64,
-                (kind, device),
-            ),
+            x_space: Array::linspace(x_min, x_max, n_per_dim),
             r,
         }
     }
 
-    fn x(&self, state: &HypergridState) -> Tensor {
-        self.x_space.gather(0, &state.coordinate, false)
+    fn x(&self, state: &HypergridState) -> ArrayView1<f64> {
+        self.x_space.slice::<Ix1>(state.coordinate)
     }
 }
 
-fn corners(x: &Tensor) -> Tensor {
+fn corners(x: &ArrayView1<f64>) -> f64 {
     // return (ax > 0.5).prod(-1) * 0.5 + ((ax < 0.8) * (ax > 0.6)).prod(-1) * 2
     // + r_0
     // x.gt(&0.5).all().into() as f64 * 0.5
-    Tensor::from(0.0)
+    0.0
 }
 
 #[cfg(test)]
@@ -170,7 +146,6 @@ mod tests {
     fn test_hypergrid() {
         let grid = Hypergrid::new(2, 8, -1.0, 1.0, corners);
         let x = grid.x(&grid.s0);
-        let x0: f64 = x.get(0).into();
-        assert_eq!(x0, -1.0);
+        assert_eq!(x[0], -1.0);
     }
 }
