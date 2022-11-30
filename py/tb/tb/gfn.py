821dfac260c81944648a940428892b503af90279
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-11-29 17:35:05 -0800

    starting factory pattern

--- py/tb/tb/gfn.py
+++ py/tb/tb/gfn.py
@@ -1,8 +1,18 @@
-from core import Loss
+from typing import Iterable
+
+from core import Environment, Loss
 from data import StreamingDataset, StreamingQueueAdapter
 from models import MultiLayerPerceptron
 
 
 def worker(stream: Iterable):
     for (s, a) in stream:
+        pass
+
+
+class GFN:
+    def __init__(self):
+        raise NotImplementedError("Use GFN.build(...)")
+
+    def build(self, env: Environment, loss: Loss):
         pass
