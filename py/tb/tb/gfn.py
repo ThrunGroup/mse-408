ef4ef7d3e0967c18c673d9186f1de25906ee3dce
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-12-11 22:26:16 -0800

    updates to gfn

--- py/tb/tb/gfn.py
+++ py/tb/tb/gfn.py
@@ -2,7 +2,7 @@ from typing import Iterable
 
 import pytorch_lightning as pl
 import torch
-from core import IntoTensor, Loss, SimpleDiscreteEnvironment, Trajectories
+from core import IntoTensor, Loss, SimpleDiscreteEnvironment
 from models import MultiLayerPerceptron
 from torch import Tensor
 
@@ -18,29 +18,28 @@ class GFN(pl.LightningModule):
         if loss != Loss.TrajectoryBalance:
             raise Exception("Only TrajectoryBalance loss currently supported!")
         self._loss = self._trajectory_balance_loss
-        self._pf = MultiLayerPerceptron(env.n_states, env.n_actions)
-        self._pb = MultiLayerPerceptron(env.n_states, env.n_actions)
+        self._log_pf = MultiLayerPerceptron(env.n_states, env.n_actions)
+        self._log_pb = MultiLayerPerceptron(env.n_states, env.n_actions)
         self._log_z = torch.tensor(0)
 
-    def _trajectory_balance_loss(self, trajectories: Trajectories) -> Tensor:
-        trajs = []
-        actions = []
-        rewards = []
-        for trajectory in trajectories:
-            actions = []
-            states = []
-            for step in trajectory:
-                actions.append(step.transition.action)
-                states.append(step.transition.state.into_tensor())
-        log_f = self._pf(trajectories)
-        log_b = self._pb(trajectories)
-        log_r = torch.log(rewards)
-        return torch.square(self._log_z + log_f - log_b - log_r)
+    def _trajectory_balance_loss(self, batch) -> Tensor:
+        # m trajectories
+        # n_i = length of trajectory i
+        # |batch.trajectory_id| = sum_i (n_i-1)
+        # |batch.states| = sum_i n_i-1
+        # |batch.actions| = sum_i n_i-1
+        # |batch.rewards| = m
+        log_f = self._log_pf(batch.states)[batch.actions].gather(0, batch.tid).sum()
+        log_b = self._log_pb(batch.states)[batch.actions].gather(0, batch.tid).sum()
+        log_r = torch.log(batch.rewards)
+        return (self._log_z + log_f - log_b - log_r).pow(2).mean()
 
     def forward(self, states: list[IntoTensor]) -> Tensor:
-        return self._pf(torch.tensor([s.into_tensor() for s in states]))
+        # TODO(danj): do we want flows or probabilities?
+        return self._log_pf(torch.tensor([s.into_tensor() for s in states]))
 
     def training_step(self, batch, _batch_idx) -> Tensor:
+        # TODO: dataset class needs to prepare data in tensor format
         return self._loss(batch)
 
     def configure_optimizers(self):
