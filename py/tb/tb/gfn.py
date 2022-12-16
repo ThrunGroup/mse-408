aadb6285203a795a906a868253f3fad7a4bc257f
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-12-16 09:06:23 -0800

    updates to gfn

--- py/tb/tb/gfn.py
+++ py/tb/tb/gfn.py
@@ -1,15 +1,13 @@
-from typing import Iterable
-
 import pytorch_lightning as pl
 import torch
 from core import IntoTensor, Loss, SimpleDiscreteEnvironment
+from data import TrajectoryTensor
 from models import MultiLayerPerceptron
 from torch import Tensor
 
-
-def worker(stream: Iterable):
-    for (s, a) in stream:
-        pass
+# TODO(danj): How to interleave on-policy sampling with train_dataloader
+# TODO(danj): Make GFN a PolicyService
+# TODO(danj): How do you define flow through abitrary points?
 
 
 class GFN(pl.LightningModule):
@@ -22,15 +20,9 @@ class GFN(pl.LightningModule):
         self._log_pb = MultiLayerPerceptron(env.n_states, env.n_actions)
         self._log_z = torch.tensor(0)
 
-    def _trajectory_balance_loss(self, batch) -> Tensor:
-        # m trajectories
-        # n_i = length of trajectory i
-        # |batch.trajectory_id| = sum_i (n_i-1)
-        # |batch.states| = sum_i n_i-1
-        # |batch.actions| = sum_i n_i-1
-        # |batch.rewards| = m
-        log_f = self._log_pf(batch.states)[batch.actions].gather(0, batch.tid).sum()
-        log_b = self._log_pb(batch.states)[batch.actions].gather(0, batch.tid).sum()
+    def _trajectory_balance_loss(self, batch: TrajectoryTensor) -> Tensor:
+        log_f = self._log_pf(batch.states)[batch.actions].gather(0, batch.ids).sum()
+        log_b = self._log_pb(batch.states)[batch.actions].gather(0, batch.ids).sum()
         log_r = torch.log(batch.rewards)
         return (self._log_z + log_f - log_b - log_r).pow(2).mean()
 
